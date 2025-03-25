96a769450935e865f870118ed3fb0a82ab684539
Javi <jemonje@correo.url.edu.gt>
2025-03-25 00:35:17 -0600

    TestCase 3 arreglado

--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -1,9 +1,31 @@
 package ed.lab;
+import java.util.List;
+import java.util.LinkedList;
+
 
 public class E02KthSmallest {
 
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        if(root==null)
+            return 0;
+
+        return inorderTraversal(root).get(k-1);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if(root==null)
+            return result;
+
+        traverser(root, result);
+        return result;
     }
 
+    private void traverser(TreeNode<Integer> root, List<Integer> result) {
+        if (root == null)
+            return;
+
+        traverser(root.left, result);
+        result.add(root.value);
+        traverser(root.right, result);
+    }
 }
